a7ab94f8084bfff594c78e86eb3a8db8d3548952
lucas <lucassidpacheco@gmail.com>
2016-04-15 01:37:06 -0300

    bugs fixed

--- src/com/crapgames/calourosimulator/assets/FXML/fxmlCaller.java
+++ src/com/crapgames/calourosimulator/assets/FXML/fxmlCaller.java
@@ -1,10 +1,42 @@
 package com.crapgames.calourosimulator.assets.FXML;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 /**
  * Created by lucas on 15/04/16.
  */
 public class fxmlCaller {
-    public void introduction(){
-        
+    public  Parent introduction() throws IOException{
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("Introduction.fxml"));
+
+        return root;
+    }
+    public  Parent lost() throws IOException{
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("Lost.fxml"));
+
+        return root;
+    }
+    public  Parent menuCreate() throws IOException{
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("MenuCreate.fxml"));
+
+        return root;
+    }
+    public  Parent about() throws IOException{
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("About.fxml"));
+
+        return root;
+    }
+    public  Parent mainMenu() throws IOException{
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+        return root;
     }
 }
