f611ea7e06f0dc75d80dfa6bb4b8da2daa749cf8
Lucas Pacheco <13892414+lsiddd@users.noreply.github.com>
2024-07-05 11:25:42 -0300

    Ressuscitar nosso bb :)

--- src/com/crapgames/calourosimulator/views/fxmlCaller.java
+++ src/com/crapgames/calourosimulator/views/fxmlCaller.java
@@ -6,61 +6,57 @@ import javafx.scene.Parent;
 
 import java.io.IOException;
 
-/**
- * Created by lucas on 15/04/16.
- */
 public class fxmlCaller {
     DbSaver see = new DbSaver();
 
-    public Parent select() throws IOException{
-        if(Integer.valueOf(see.getLvl(see.getCurrent())) == 1)
-            return FXMLLoader.load(getClass().getResource("Introduction.fxml"));
-
+    public Parent select() throws IOException {
+        if (Integer.valueOf(see.getLvl(see.getCurrent())) == 1)
+            return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/Introduction.fxml"));
         else
-            return FXMLLoader.load(getClass().getResource("arc1-1.fxml"));
+            return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/arc1-1.fxml"));
     }
 
-    public  Parent introduction() throws IOException{
-        return FXMLLoader.load(getClass().getResource("Introduction.fxml"));
+    public Parent introduction() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/Introduction.fxml"));
     }
 
-    public  Parent lost() throws IOException{
-        return FXMLLoader.load(getClass().getResource("menus/Lost.fxml"));
+    public Parent lost() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/menus/Lost.fxml"));
     }
 
-    public  Parent menuCreate() throws IOException{
-        return FXMLLoader.load(getClass().getResource("menus/MenuCreate.fxml"));
+    public Parent menuCreate() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/menus/MenuCreate.fxml"));
     }
 
-    public  Parent about() throws IOException{
-        return FXMLLoader.load(getClass().getResource("menus/About.fxml"));
+    public Parent about() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/menus/About.fxml"));
     }
 
-    public  Parent mainMenu() throws IOException{
-        return FXMLLoader.load(getClass().getResource("menus/MainMenu.fxml"));
+    public Parent mainMenu() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/menus/MainMenu.fxml"));
     }
 
-    public Parent mathScene() throws IOException{
-        return FXMLLoader.load(getClass().getResource("arc1/math.fxml"));
+    public Parent mathScene() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/arc1/math.fxml"));
     }
 
-    public Parent phyScene() throws IOException{
-        return FXMLLoader.load(getClass().getResource("arc1/physicScene.fxml"));
+    public Parent phyScene() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/arc1/physicScene.fxml"));
     }
 
-    public Parent qmcScene() throws IOException{
-        return FXMLLoader.load(getClass().getResource("arc1/qmc/qmcScene.fxml"));
+    public Parent qmcScene() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/arc1/qmc/qmcScene.fxml"));
     }
 
-    public Parent qmcScene1() throws IOException{
-        return FXMLLoader.load(getClass().getResource("arc1/qmc/qmcScene-1.fxml"));
+    public Parent qmcScene1() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/arc1/qmc/qmcScene-1.fxml"));
     }
 
-    public Parent splashScreen() throws IOException{
-        return FXMLLoader.load(getClass().getResource("SplashScene.fxml"));
+    public Parent splashScreen() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/SplashScene.fxml"));
     }
 
-    public Parent endGame() throws IOException{
-        return FXMLLoader.load(getClass().getResource("EndGame.fxml"));
+    public Parent endGame() throws IOException {
+        return FXMLLoader.load(getClass().getResource("/com/crapgames/calourosimulator/views/EndGame.fxml"));
     }
 }
